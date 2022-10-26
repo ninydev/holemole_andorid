7745e577d3a99d02a6836a9679f26463c56f9a24
Oleksandr Nykytin <keeper@ninydev.com>
2022-10-26 19:40:32 +0300

    Hole & Mole

--- app/src/main/java/com/itstep/holemole/MainActivity.java
+++ app/src/main/java/com/itstep/holemole/MainActivity.java
@@ -1,8 +1,10 @@
 package com.itstep.holemole;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout gamePad = findViewById(R.id.gamePad);
+
+        ImageView hole = new ImageView(this);
+        hole.setImageResource(R.drawable.hole);
+
+
+        gamePad.addView(hole);
+
     }
 }
